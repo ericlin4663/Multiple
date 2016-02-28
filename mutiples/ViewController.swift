08a8f85e2d101cf2ab47d1e6019b26e5ee303e02
Eric Lin <ericlin@Erics-MacBook-Pro.local>
2016-02-28 15:22:06 -0500

    multiple

--- mutiples/ViewController.swift
+++ mutiples/ViewController.swift
@@ -10,16 +10,67 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var titleImage: UIImageView!
+    @IBOutlet weak var mutipleTextField: UITextField!
+    @IBOutlet weak var playButton: UIButton!
+    
+    
+    @IBOutlet weak var sumLabel: UILabel!
+    @IBOutlet weak var addButton: UIButton!
+    
+    var firstNum = 0
+    var secondNum = 0
+    var sum = 0
+    let FIVE_TIMES = 5
+    var loopFiveTimes = 0
+
+    @IBAction func playButtonPressed(sender: UIButton) {
+        if mutipleTextField.text != nil && mutipleTextField.text != "" {
+            secondNum = Int(mutipleTextField.text!)!
+            
+            titleImage.hidden = true
+            mutipleTextField.hidden = true
+            playButton.hidden = true
+            
+            sumLabel.hidden = false
+            addButton.hidden = false
+           
+            sumLabel.text = "Press ADD to add"
+         //   updateMultipleLabel()
+        }
+        
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func addButtonPressed(sender: UIButton) {
+        
+        if loopFiveTimes++ <= FIVE_TIMES {
+            updateMultipleLabel()
+        }
+        else {
+            gameRestart()
+        }
     }
-
-
+    
+    func gameRestart () {
+        titleImage.hidden = false
+        mutipleTextField.hidden = false
+        playButton.hidden = false
+        sumLabel.hidden = true
+        addButton.hidden = true
+        
+        mutipleTextField.text = ""
+        sumLabel.text = ""
+        firstNum = 0
+        secondNum = 0
+        sum = 0
+        loopFiveTimes = 0
+        
+    }
+    
+    func updateMultipleLabel () {
+        sumLabel.text = "\(firstNum) + \(secondNum) = \(firstNum + secondNum)"
+        firstNum += secondNum
+    }
 }
 
